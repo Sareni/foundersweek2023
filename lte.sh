156bfec301a299cf2bb7410fb0019e6b79d846f2
Martin Limberger <martin.limberger@gmx.net>
2023-09-01 10:31:18 +0200

    add lte.sh

---
+++ lte.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+MODEM_IP="192.168.8.1"
+curl -s -X GET "http://$MODEM_IP/api/webserver/SesTokInfo" > ses_tok.xml
+COOKIE=`grep "SessionID=" ses_tok.xml | cut -b 10-147`
+TOKEN=`grep "TokInfo" ses_tok.xml | cut -b 10-41`
+CONNECT_REQ="<request><dataswitch>1</dataswitch></request>"
+
+curl -X POST -d $CONNECT_REQ "http://$MODEM_IP/api/dialup/mobile-dataswitch" \
+-c $COOKIE --header "__RequestVerificationToken: $TOKEN" \
+--header "Content-Type: text/xml" --dump-header login_resp_hdr.txt > /dev/null
